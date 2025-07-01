dd4e2cd1fb17708e821f2834149cafdd11644e99
晁悦 <165813645+cymilucymilu@users.noreply.github.com>
2025-07-01 21:56:14 +0800

    Add files via upload

---
+++ sh01/js.js
@@ -0,0 +1,36 @@
+new Vue({
+    el: "#app",
+    data: {
+        users: [
+            {
+                name: "Tom",
+                age: 20,
+                gender: 1,
+                score: 78
+            },
+            {
+                name: "Rose",
+                age: 18,
+                gender: 2,
+                score: 86
+            },
+            {
+                name: "Jerry",
+                age: 26,
+                gender: 1,
+                score: 90
+            },
+            {
+                name: "Tony",
+                age: 30,
+                gender: 1,
+                score: 52
+            }
+        ]
+    },
+    methods: {
+        sortByScore() {
+            this.users.sort((a, b) => b.score - a.score);
+        }
+    }
+})
